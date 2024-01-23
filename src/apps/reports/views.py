798c8ee50ac8afcb5fc6613deb430d0d6e0cb15c
wcreativo <w.kreativo@gmail.com>
2024-01-22 23:53:08 -0500

    Reading CSV file

--- src/apps/reports/views.py
+++ src/apps/reports/views.py
@@ -1,3 +1,5 @@
+import csv
+
 from django.http import HttpResponse
 from rest_framework import status
 from rest_framework.response import Response
@@ -33,3 +35,30 @@ class ClientsReportView(APIView):
             {"message": "There're not clients on database"},
             status=status.HTTP_400_BAD_REQUEST,
         )
+
+    def post(self, request):
+        if "file" not in request.FILES:
+            return Response(
+                {"error": "There is not a CSV file in the request."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        csv_file = request.FILES["file"]
+
+        if not csv_file.name.endswith(".csv"):
+            return Response(
+                {"error": "The file must be CSV."}, status=status.HTTP_400_BAD_REQUEST
+            )
+
+        try:
+            with csv_file.open(mode="r") as file:
+                decoded_file = csv_file.read().decode("utf-8").splitlines()
+                csv_reader = csv.DictReader(decoded_file)
+                data = list(csv_reader)
+        except Exception as e:
+            return Response(
+                {"error": f"Failed to process the CSV file. Error: {str(e)}"},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
+
+        return Response({"message": "Archivo CSV procesado exitosamente."})
--- src/config/settings/base.py
+++ src/config/settings/base.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -104,5 +105,19 @@ REST_FRAMEWORK = {
     ],
 }
 SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ROTATE_REFRESH_TOKENS": False,
+    "BLACKLIST_AFTER_ROTATION": True,
+    "ALGORITHM": "HS256",
+    "VERIFYING_KEY": None,
     "AUTH_HEADER_TYPES": ("Bearer",),
+    "USER_ID_FIELD": "id",
+    "USER_ID_CLAIM": "user_id",
+    "AUTH_TOKEN_CLASSES": ("rest_framework_simplejwt.tokens.AccessToken",),
+    "TOKEN_TYPE_CLAIM": "token_type",
+    "JTI_CLAIM": "jti",
+    "SLIDING_TOKEN_REFRESH_EXP_CLAIM": "refresh_exp",
+    "SLIDING_TOKEN_LIFETIME": timedelta(minutes=5),
+    "SLIDING_TOKEN_REFRESH_LIFETIME": timedelta(days=1),
 }
